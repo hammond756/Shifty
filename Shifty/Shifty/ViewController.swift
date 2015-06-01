5f5ecf2108df730c93aa2d138066d2f6e6de2d2d
aronhammond <hammond756@live.nl>
2015-06-01 17:30:38 +0200

    Created empty Xcode project

---
+++ Shifty/Shifty/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Shifty
+//
+//  Created by Aron Hammond on 01/06/15.
+//  Copyright (c) 2015 Aron Hammond. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
