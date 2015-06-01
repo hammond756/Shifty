2281696d7ac62b3e1e707fbbf26693e3c76b7ca7
aronhammond <hammond756@live.nl>
2015-06-01 22:02:43 +0200

    Added ViewController classes for each view

---
+++ Shifty/Shifty/GezochtViewController.swift
@@ -0,0 +1,35 @@
+//
+//  GezochtViewController.swift
+//  Shifty
+//
+//  Created by Aron Hammond on 01/06/15.
+//  Copyright (c) 2015 Aron Hammond. All rights reserved.
+//
+
+import UIKit
+
+class GezochtViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
